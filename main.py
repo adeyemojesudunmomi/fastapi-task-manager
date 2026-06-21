0ab20f5a207814f72985389b121865d374c46c1c
Nimi <adeyemojesudunmomi@gmail.com>
2026-06-21 10:57:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,106 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+import models
+import schemas
+from database import engine, SessionLocal
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Task Manager API")
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
 def home():
     return {"message": "Task Manager API Running"}
+
+
+@app.post("/tasks", response_model=schemas.TaskResponse)
+def create_task(task: schemas.TaskCreate,
+                db: Session = Depends(get_db)):
+
+    new_task = models.Task(
+        title=task.title,
+        description=task.description
+    )
+
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+
+    return new_task
+
+
+@app.get("/tasks")
+def get_tasks(db: Session = Depends(get_db)):
+    return db.query(models.Task).all()
+
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int,
+             db: Session = Depends(get_db)):
+
+    task = db.query(models.Task).filter(
+        models.Task.id == task_id
+    ).first()
+
+    if not task:
+        raise HTTPException(
+            status_code=404,
+            detail="Task not found"
+        )
+
+    return task
+
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int,
+                db: Session = Depends(get_db)):
+
+    task = db.query(models.Task).filter(
+        models.Task.id == task_id
+    ).first()
+
+    if not task:
+        raise HTTPException(
+            status_code=404,
+            detail="Task not found"
+        )
+
+    task.completed = True
+
+    db.commit()
+
+    return {
+        "message": "Task marked as completed"
+    }
+
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int,
+                db: Session = Depends(get_db)):
+
+    task = db.query(models.Task).filter(
+        models.Task.id == task_id
+    ).first()
+
+    if not task:
+        raise HTTPException(
+            status_code=404,
+            detail="Task not found"
+        )
+
+    db.delete(task)
+    db.commit()
+
+    return {
+        "message": "Task deleted successfully"
+    }
